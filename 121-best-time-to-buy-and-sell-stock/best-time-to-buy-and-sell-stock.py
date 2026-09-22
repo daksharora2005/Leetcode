aa59da86c0babd5b0b3f2991d766404bfdd1aa48
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-23 02:03:58 +0530

    Time: 27 ms (95.92%) | Memory: 19 MB (50.43%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -2,31 +2,12 @@ class Solution(object):
     def maxProfit(self, prices):
         if not prices:
             return 0
-
-        global_min = prices[0]
-        global_max = prices[0]
-
+        mp = float('inf')
+        mx = 0
         for price in prices:
-            if price < global_min:
-                global_min = price
-            if price > global_max:
-                global_max = price
-
-        upper_bound = global_max - global_min
-
-        lowest = prices[0]
-        m = 0
-
-        for price in prices:
-            if price < lowest:
-                lowest = price
-
-            profit = price - lowest
-
-            if profit > m:
-                m = profit
-
-                if m == upper_bound:
-                    return m
-
-        return m
+            if price < mp:
+                mp = price
+            elif price - mp > mx:
+                mx = price - mp
+                
+        return mx
