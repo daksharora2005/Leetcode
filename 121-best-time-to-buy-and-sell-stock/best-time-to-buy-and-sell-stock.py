55f5aa3fc919d7be0321f0a89031dab83a5ed1c0
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-23 02:07:11 +0530

    Time: 39 ms (77.77%) | Memory: 18.9 MB (76.37%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
 class Solution(object):
     def maxProfit(self, prices):
-        max_profit=0
-        max_price=0
-        while prices:
-            price=prices.pop()
-
-            if price>max_price:
-                max_price=price
-            else:
-                max_profit=max(max_profit,max_price-price)
-        return max_profit
+        mp = float('inf')
+        mx = 0
+        for price in prices:
+            if price < mp:
+                mp = price
+            elif price - mp > mx:
+                mx = price - mp
+                
+        return mx
