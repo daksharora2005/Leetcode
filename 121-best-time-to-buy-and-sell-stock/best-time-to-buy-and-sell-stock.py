989a039cda22df90ec225558cf741a5ee763da86
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-23 02:04:52 +0530

    Time: 27 ms (95.92%) | Memory: 19 MB (50.43%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def maxProfit(self, prices):
-        mp = float('inf')
-        mx = 0
+        mp=float('inf')
+        mx=0
         for price in prices:
-            if price < mp:
-                mp = price
-            elif price - mp > mx:
-                mx = price - mp
-                
+            if price<mp:
+                mp=price
+            elif price-mp>mx:
+                mx=price-mp    
         return mx
