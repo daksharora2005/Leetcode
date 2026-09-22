40ef7fdec0b3ca64dd798544f32a01c8d555ed55
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-23 02:01:33 +0530

    Time: 35 ms (83.45%) | Memory: 19 MB (76.37%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,24 +1,32 @@
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
         if not prices:
             return 0
-        upper_bound=max(prices)-min(prices)
-        lowest=prices[0]
-        m=0
+
+        global_min = prices[0]
+        global_max = prices[0]
+
         for price in prices:
-            lowest=min(lowest, price)
-            profit=price-lowest
-            m=max(m,profit)
-            if m==upper_bound:
-                return m
+            if price < global_min:
+                global_min = price
+            if price > global_max:
+                global_max = price
+
+        upper_bound = global_max - global_min
+
+        lowest = prices[0]
+        m = 0
+
+        for price in prices:
+            if price < lowest:
+                lowest = price
+
+            profit = price - lowest
+
+            if profit > m:
+                m = profit
+
+                if m == upper_bound:
+                    return m
 
         return m
-
-
-
-            
-
