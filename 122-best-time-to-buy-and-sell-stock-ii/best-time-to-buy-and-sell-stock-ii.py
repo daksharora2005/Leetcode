a635e2d06b1b67353b72bb302df7ff0036216bb6
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-24 17:36:10 +0530

    Time: 6 ms (19.96%) | Memory: 13.9 MB (8.71%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        min_p=prices[0]
         mp=0
         for i in range(1,len(prices)):
-            if prices[i]>min_p:
-                mp+=prices[i]-min_p
-                min_p=prices[i]
-            min_p=prices[i]
+            if prices[i]>prices[i-1]:
+                mp+=prices[i]-prices[i-1]
         return mp
 
             
