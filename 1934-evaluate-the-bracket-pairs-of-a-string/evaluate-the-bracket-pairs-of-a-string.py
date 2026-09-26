3231db85ddcfa827b8b4c199b1f901fefb13a0c6
Daksh Arora <128375165+daksharora2005@users.noreply.github.com>
2026-09-26 17:29:18 +0530

    Time: 75 ms (60.71%) | Memory: 56 MB (28.57%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -5,16 +5,22 @@ class Solution(object):
         :type knowledge: List[List[str]]
         :rtype: str
         """
-        for i in knowledge:
-            s=s.replace("("+i[0]+")",i[1])
-        i=0
-        while i<len(s):
-            if s[i]=="(":
-                j=s.find(")",i)
-                s=s[:i] + "?" + s[j+1:]
+        mp = dict(knowledge)
+        ans = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = s.index(')', i)
+                key = s[i + 1:j]
+                ans.append(mp.get(key, '?'))
+                i = j + 1
             else:
-                i+=1
-        return s
+                ans.append(s[i])
+                i += 1
+
+        return ''.join(ans)
+                
             
 
 
